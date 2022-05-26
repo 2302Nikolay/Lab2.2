8eda18014b1c22b8f948b073e55790f3ee63bbc7
2302Nikolay <Malzewn@gmail.com>
2022-05-26 13:16:57 +0300

    Мелкие исправления

--- Решение индивидуального задания/Task3_var11.py
+++ Решение индивидуального задания/Task3_var11.py
@@ -3,7 +3,7 @@
 
 if __name__ == '__main__':
 
-    for i in range(1, 10):
-        for j in range(1, 10):
+    for i in range(1, 11):
+        for j in range(1, 11):
             print(i, "*", j, "=", i*j, "\n")
         print("\n")
